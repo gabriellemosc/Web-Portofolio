e6ff537dcf29ac2f9bb119e6243be1ff90695010
gabriellemosc <gabriel.palacios1910@gmail.com>
2025-02-17 17:19:54 -0300

    Text about me

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -42,83 +42,81 @@ const TAB_DATA = [
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
-  const [isPending, startTransition] = useTransition();
-  const [isVisible, setIsVisible] = useState(false);
-  const aboutRef = useRef(null);
+  const [isTitleVisible, setIsTitleVisible] = useState(false);
+  const [isTextVisible, setIsTextVisible] = useState(false);
+  
+  const titleRef = useRef(null);
+  const textRef = useRef(null);
 
   const handleTabChange = (id) => {
-    startTransition(() => {
-      setTab(id);
-    });
+    setTab(id);
   };
 
-  // Observer para detectar quando o título entra em vista
+  // Observer para o título
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        setIsVisible(entry.isIntersecting);
+        setIsTitleVisible(entry.isIntersecting);
       },
-      {
-        threshold: 0.5, // O título será considerado visível quando 50% dele estiver na tela
-      }
+      { threshold: 0.5 }
     );
 
-    if (aboutRef.current) {
-      observer.observe(aboutRef.current);
-    }
+    if (titleRef.current) observer.observe(titleRef.current);
 
     return () => {
-      if (aboutRef.current) {
-        observer.unobserve(aboutRef.current);
-      }
+      if (titleRef.current) observer.unobserve(titleRef.current);
+    };
+  }, []);
+
+  // Observer para o texto
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsTextVisible(entry.isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
+
+    if (textRef.current) observer.observe(textRef.current);
+
+    return () => {
+      if (textRef.current) observer.unobserve(textRef.current);
     };
   }, []);
 
   return (
-    <section
-      className="text-white"
-      id="about"
-    >
+    <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2
-            ref={aboutRef}
-            className={`about ${isVisible ? "animate-visible" : ""}`} // Animação do título
+            ref={titleRef}
+            className={`about ${isTitleVisible ? "animate-visible" : ""}`}
           >
             About Me
           </h2>
-          <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+          <p
+            ref={textRef}
+            className={`text-base lg:text-lg about_text ${isTextVisible ? "animate-visible" : ""}`}
+          >
+            I am a full stack web developer with a passion for creating interactive and responsive 
+            web applications. I have experience working with JavaScript, React, Redux, Node.js, 
+            Express, PostgreSQL, Sequelize, HTML, CSS, and Git. I am a quick learner and I am 
+            always looking to expand my knowledge and skill set. I am a team player and I am excited 
+            to work with others to create amazing applications.
           </p>
           <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
+            <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
               Skills
             </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
+            <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
               Education
             </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
+            <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>
               Certifications
             </TabButton>
           </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
+          <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
         </div>
       </div>
     </section>
