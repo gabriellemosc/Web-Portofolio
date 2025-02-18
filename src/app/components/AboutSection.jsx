1dc7c4bcf5eddfc3036a3948f1f6dc81a507e62e
gabriellemosc <gabriel.palacios1910@gmail.com>
2025-02-18 18:41:40 -0300

    About SVG finished

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -5,18 +5,18 @@ import TabButton from "./TabButton";
 
 // Array com os ícones das ferramentas de programação
 const TOOL_ICONS = [
-  { name: "", src: "/noda_js.svg" },
-  { name: "", src: "/docker.svg" },
-  { name: "", src: "/python.svg" },
-  { name: "", src: "/linux.svg" },
-  { name: "", src: "/bash.svg" },
-  { name: "", src: "/aws.svg" },
-  { name: "", src: "/cpp.svg" },
-  { name: "", src: "/js.svg" },
-  { name: "", src: "/mysql.svg" },
-  { name: "", src: "/git.svg" },
-  { name: "", src: "/apache.svg" },
-  { name: "", src: "/django.svg" },
+  { name: "Node.js", src: "/noda_js.svg" },
+  { name: "Docker", src: "/docker.svg" },
+  { name: "Python", src: "/python.svg" },
+  { name: "Linux", src: "/linux.svg" },
+  { name: "Bash", src: "/bash.svg" },
+  { name: "AWS", src: "/aws.svg" },
+  { name: "C++", src: "/cpp.svg" },
+  { name: "JS", src: "/js.svg" },
+  { name: "MySQL", src: "/mysql.svg" },
+  { name: "Git", src: "/git.svg" },
+  { name: "Apache", src: "/apache.svg" },
+  { name: "Django", src: "/django.svg" },
 ];
 
 const AboutSection = () => {
@@ -70,8 +70,12 @@ const AboutSection = () => {
             }`}
             style={{ transitionDelay: `${index * 100}ms` }}
           >
-            <Image src={tool.src} alt={tool.name} width={50} height={50} />
-            <p className="text-sm mt-2">{tool.name}</p>
+            <div className="relative group">
+              <Image src={tool.src} alt={tool.name} width={50} height={50} />
+              <span className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-sm mt-2 text-white">
+                {tool.name}
+              </span>
+            </div>
           </div>
         ))}
       </div>
@@ -110,31 +114,41 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 ">
-      <Image
-            src="/images/about-image.png"
-            width={500}
-            height={500}
-            className={`about-image ${isTitleVisible ? "animate-visible" : ""}`}
-          />
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          className={`about-image ${isTitleVisible ? "animate-visible" : ""}`}
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 ref={titleRef} className={`about ${isTitleVisible ? "animate-visible" : ""}`}>
             About Me
           </h2>
-          <p
+          <div
             ref={textRef}
             className={`text-base lg:text-lg about_text ${isTextVisible ? "animate-visible" : ""}`}
           >
-            I am a full stack web developer with a passion for creating interactive and responsive
-            web applications. I have experience working with JavaScript, React, Redux, Node.js,
-            Express, PostgreSQL, Sequelize, HTML, CSS, and Git. I am a quick learner and I am
-            always looking to expand my knowledge and skill set. I am a team player and I am excited
-            to work with others to create amazing applications.
-          </p>
+            <p className="about_p">
+              I am a <strong>backend developer</strong> from <i>Sao Paulo, Brazil</i>, passionate about{" "}
+              <strong>technology</strong> and solving <strong>complex problems</strong>. With experience in software
+              development, I focus on creating <strong>scalable, efficient, and secure</strong> systems that drive innovation.
+            </p>
+
+            <p className="about_p">
+              My expertise includes <strong>C++, Python, and JavaScript</strong>, which I use to build high-performance
+              applications. I also work with <strong>Docker, Linux, and Bash</strong> to create flexible environments and
+              automate processes, streamlining development and infrastructure.
+            </p>
+
+            <p className="about_p">
+              Leveraging <strong>AWS</strong> for cloud scalability, <strong>MySQL</strong> for database management, and{" "}
+              <strong>Apache</strong> for web performance optimization, I ensure that applications run smoothly and
+              efficiently.
+            </p>
+          </div>
           <div
             ref={topicsRef}
-            className={`flex flex-row justify-start mt-8 topics_about ${
-              isTopicsVisible ? "animate-visible" : ""
-            }`}
+            className={`flex flex-row justify-start mt-8 topics_about ${isTopicsVisible ? "animate-visible" : ""}`}
           >
             <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
               Skills
@@ -142,10 +156,7 @@ const AboutSection = () => {
             <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
               Education
             </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
+            <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>
               Certifications
             </TabButton>
           </div>
