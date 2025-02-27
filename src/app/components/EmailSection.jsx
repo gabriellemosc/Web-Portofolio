2f41a37da1de7fb7efe628e1b140905ca821c1e2
gabriellemosc <gabriel.palacios1910@gmail.com>
2025-02-27 08:37:51 -0300

    Texto email section

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -91,9 +91,10 @@ const EmailSection = () => {
         {/* Bloco de texto animado */}
         <div className="z-10">
           <h5 className="text-xl font-bold text-white my-2 lets-connect">Let&apos;s Connect</h5>
-          <p className="text-[#ADB7BE] mb-4 max-w-md">
-            I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you!
-          </p>
+          <p className="text-[#ADB7BE] mb-4 max-w-md text-email-section">
+  <span className="span-text-email-section">Let's Talk.</span> I'm always open to new connections! Whether it's for career opportunities or a tech discussion, feel free to reach out. If you’re interested and enjoyed my work, I’d love to chat!
+</p>
+
           <div className="socials flex flex-row gap-2">
             <Link href="https://github.com">
               <Image src={GithubIcon} alt="Github Icon" />
